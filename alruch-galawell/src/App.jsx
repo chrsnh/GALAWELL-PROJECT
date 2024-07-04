5b98b2bc8b1cbd19982055d4f5ca5ce04c3d7941
chrsnh <chao@gbox.adnu.edu.ph>
2024-07-04 18:35:21 +0800

    first commit

---
+++ alruch-galawell/src/App.jsx
@@ -0,0 +1,54 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Blog from "./pages/blog/Blog";
+import AllBlogs from "./pages/allBlogs/AllBlogs";
+import Nopage from "./pages/nopage/Nopage";
+import BlogInfo from "./pages/blogInfo/BlogInfo";
+import AdminLogin from "./pages/admin/adminLogin/AdminLogin";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import CreateBlog from "./pages/admin/createBlog/createBlog";
+import MyState from "./context/data/myState";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <div>
+      <MyState>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/allblogs" element={<AllBlogs />} />
+            <Route path="/bloginfo/:id" element={<BlogInfo />} />
+            <Route path="/adminlogin" element={<AdminLogin />} />
+
+            <Route path="/dashboard" element={
+              <ProtectedRouteForAdmin>
+                <Dashboard />
+              </ProtectedRouteForAdmin>
+            } />
+            <Route path="/createblog" element={
+              <ProtectedRouteForAdmin>
+                <CreateBlog />
+              </ProtectedRouteForAdmin>
+              } />
+            <Route path="/*" element={<Nopage />} />
+          </Routes>
+          <Toaster/>
+        </Router>
+      </MyState>
+    </div>
+  )
+}
+
+export default App
+
+export const ProtectedRouteForAdmin = ({ children }) => {
+  const admin = JSON.parse(localStorage.getItem('admin'))
+  if (admin?.user?.email === "chao@gbox.adnu.edu.ph") {
+    return children
+  }
+  else {
+    return <Navigate to={'/adminlogin'}/>
+  }
+}
---
+++ alruch-galawell/src/pages/blog/Blog.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Blog() {
+    return (
+        <div> BLOG </div>
+    )
+}
+
+export default Blog
